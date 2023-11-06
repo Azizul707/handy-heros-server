56a188d0d8d0a1edda922028b6f2561b86896fc5
Azizul <arafinazizul@gmail.com>
2023-11-07 02:32:10 +0600

    All initials files are added and installed all pakeges

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const cors = require( 'cors' );
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const port = process.env.PORT || 5000;
+
+app.use( express.json() );
+app.use( cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen( port, () => {
+    console.log( `Handy heros app is running on port ${ port }` )
+} );
+
+
+require('dotenv').config();
+
+
+const uri = `mongodb+srv://${process.env.DB_ACCESS_KEY}:${process.env.DB_PASS}@cluster0.hg5h0d4.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
